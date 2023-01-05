9f6b8bed09b5f83f35e0d29b5ca4e90d4f94504b
Danny <dgr73@cornell.edu>
2023-01-04 23:40:20 -0500

    t

--- portfolio-v2/vite.config.js
+++ portfolio-v2/vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio" : "./",
+  base: "/portfolio-website-v2",
   plugins: [
     inject({
       // => that should be first under plugins array
