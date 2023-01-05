10fa8e814f228945a92da271d0e1813c14fb5892
Danny <dgr73@cornell.edu>
2023-01-04 23:03:09 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/info4340-2022fa/deploy/sw.js', { scope: '/info4340-2022fa/deploy/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/portfolio/sw.js', { scope: '/portfolio/' })})}
