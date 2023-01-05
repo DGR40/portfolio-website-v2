b065ef06438133686a1c8f716bb88d23df5bcd18
Danny <dgr73@cornell.edu>
2023-01-04 23:22:59 -0500

    commit

--- portfolio-v2/vite.config.js
+++ portfolio-v2/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import inject from "@rollup/plugin-inject";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/portfolio" : "./",
   plugins: [
     inject({
       // => that should be first under plugins array
@@ -11,5 +13,39 @@ export default defineConfig({
       jQuery: "jquery",
     }),
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "PWA Example",
+        short_name: "PWAExample",
+        description: "An example Vue.js+Vite PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
 });
