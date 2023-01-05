4dc61ea5d76e9ff735f8df7b9d6cbc95a58da406
Danny <dgr73@cornell.edu>
2023-01-05 00:02:05 -0500

    test

--- portfolio-v2/vite.config.js
+++ portfolio-v2/vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-website-v2",
+  base: "/clinquant-baklava-100a8d.netlify.app/",
   plugins: [
     inject({
       // => that should be first under plugins array
